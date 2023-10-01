6b2af2ffb294ce08aeed17a68b331cc186cb9c89
Osman eser <osmaneser513@live.com>
2023-10-01 20:16:43 +0300

    update

--- GetBalance.DATA/FoodMeal.cs
+++ GetBalance.DATA/FoodMeal.cs
@@ -16,9 +16,9 @@ namespace GetBalance.DATA
 		public int FoodAmount { get; set; }
 
 		public double TotalCalori => Food.Calories * FoodAmount/(Food.PortionName==PortionName.gram?100.0:1.0);
-		public double TotalCarb => Food.Carbohydrate * FoodAmount/100;
-		public double TotalProtein => Food.Protein * FoodAmount / 100;
-		public double TotalFat => Food.Fat * FoodAmount;
+		public double TotalCarb => Food.Carbohydrate * FoodAmount/(Food.PortionName == PortionName.gram ? 100.0 : 1.0);
+		public double TotalProtein => Food.Protein * FoodAmount / (Food.PortionName == PortionName.gram ? 100.0 : 1.0);
+		public double TotalFat => Food.Fat * FoodAmount/ (Food.PortionName == PortionName.gram ? 100.0 : 1.0);
 		public Food Food { get; set; }
 		public Meal Meal { get; set; }
 	}
